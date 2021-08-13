5955b0ef5f2e333990e3eeea776548dcf18c7944
Nguyễn Nhĩ Thái <62824044+nhithai4620@users.noreply.github.com>
2021-08-13 21:35:56 +0700

    thai commit at 9:35pm 8/13/2021

--- js.js
+++ js.js
@@ -3,6 +3,8 @@ const AppleControl = document.getElementById("apple");
 const OrangeControl = document.getElementById("orange");
 const BananaControl = document.getElementById("banana");
 const StrawControl = document.getElementById("straw");
+const BackgroundLayout = document.querySelector(".hero");
+const r = document.querySelector(':root');
 
 AppleControl.addEventListener('click', () =>{AppleFunction()});
 OrangeControl.addEventListener('click', () =>{OrangeFunction()});
@@ -11,16 +13,24 @@ StrawControl.addEventListener('click', () =>{StrawFunction()});
 
 const AppleFunction = () =>{
     Fruit.src = "./img/apple-slice.png";
+    BackgroundLayout.style.backgroundImage = "url('./img/apple_bg.png')";
+    r.style.setProperty('--main-color', '#96e001');
 }
 
 const OrangeFunction = () =>{
     Fruit.src = "./img/orange-slice.png";
+    BackgroundLayout.style.backgroundImage = "url('./img/orange_bg.png')";
+    r.style.setProperty('--main-color', '#f9b033');
 }
 
 const BananaFunction = () =>{
     Fruit.src = "./img/banana-slice.png";
+    BackgroundLayout.style.backgroundImage = "url('./img/banana_bg.png')";
+    r.style.setProperty('--main-color', '#f7e544');
 }
 
 const StrawFunction = () =>{
     Fruit.src = "./img/straw-slice.png";
+    BackgroundLayout.style.backgroundImage = "url('./img/straw_bg.png')";
+    r.style.setProperty('--main-color', '#f07753');
 }
