61037ed7dda3f7e3186d9e53e6e84c61b4e43f6c
Nguyễn Nhĩ Thái <62824044+nhithai4620@users.noreply.github.com>
2021-08-17 10:05:59 +0700

    thai commit at 10:05am

--- js.js
+++ js.js
@@ -111,3 +111,18 @@ const changeSlide = (direction) => {
     slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`;
     slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`;
 }
+
+
+const Fruits = [
+  {"name":"Apple", "price":"15", "src":"./img/apple-item.png"},
+  {"name":"Banana", "price":"5", "src":"./img/banana-item.png"},
+  {"name":"Strawberries", "price":"12", "src":"./img/straw-item.png"},
+  {"name":"Melon", "price":"13", "src":"./img/melon-item.png"},
+  {"name":"Watermelon", "price":"4", "src":"./img/watermelon-item.png"},
+  {"name":"Orange", "price":"10", "src":"./img/orange-item.png"},
+  {"name":"Grape", "price":"19", "src":"./img/grape-item.png"},
+  {"name":"Durian", "price":"13", "src":"./img/durian-item.png"},
+  {"name":"Blueberries", "price":"5", "src":"./img/banana-item.png"},
+];
+
+console.log(Fruits[1].name);
